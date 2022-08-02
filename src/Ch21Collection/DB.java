0909f949cfdd16b9aefa70b5a5a6d8d8e2c44581
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-08-02 16:54:44 +0900

    DB 연동

---
+++ src/Ch21Collection/DB.java
@@ -0,0 +1,15 @@
+package Ch21Collection;
+
+public class DB {
+
+}
+
+JDK
+이클립스 WEB
+톰캣
+이클립스 -프로젝트
+이클립스 -톰캣연동
+Test Page
+Html -부트스트랩5
+Form 설정 Insert.jsp
+Insert.jsp JSP 문법 이용해서 파라미터 받기/ JDBC 코드 DB INSERT
